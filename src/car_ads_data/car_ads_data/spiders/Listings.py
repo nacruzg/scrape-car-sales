21357431eadb7bf8369d31003f449c02167d7c80
Nelson Cruz <Nelson.Cruz@woodplc.com>
2021-07-06 12:39:54 +0800

    Ensure spiders are running, remove block from site

--- src/car_ads_data/car_ads_data/spiders/Listings.py
+++ src/car_ads_data/car_ads_data/spiders/Listings.py
@@ -2,8 +2,8 @@ import scrapy
 
 class ListingsSpider(scrapy.Spider):
     name = 'Listings'
-    allowed_domains = ['https://www.carsales.com.au/cars/']
-    start_urls = ['http://https://www.carsales.com.au/cars//']
+    allowed_domains = ['www.carsales.com.au/cars/']
+    start_urls = ['www.carsales.com.au/cars/']
 
     def parse(self, response):
         pass

1af1c383f420f3e7f145838277535f247ee0c845
nelsoncruz <nelson.cruzg@gmail.com>
2021-11-21 20:58:48 +0800

    Change next_page xpath to allow recursive URLs

--- src/car_ads_data/spiders/car-sales.py
+++ src/car_ads_data/spiders/car-sales.py
@@ -30,7 +30,7 @@ class CarListingItemsSpider(scrapy.Spider):
                 'engine': key_details.xpath('//li[@data-type="Engine"]/text()').get(),
             }
 
-        next_page = response.css('li.next a::attr("href")').get()
+        next_page = response.css("ul.pagination a.next::attr(href)").get()
 
         if next_page is not None:
             yield response.follow(next_page, self.parse)
